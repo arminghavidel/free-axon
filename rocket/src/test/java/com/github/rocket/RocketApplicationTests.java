f86bfc2b003e088e3a26dee5a953ca1690682a96
armin.ghavidel <armin.ghavidel@snapppay.ir>
2024-01-06 08:40:19 +0330

    I am Groot (first commit, let's branch out!)

---
+++ rocket/src/test/java/com/github/rocket/RocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
